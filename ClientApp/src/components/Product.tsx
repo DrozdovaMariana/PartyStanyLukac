8e33a3fe103e8eca05fc1f3ae110be2625a2facd
Drozdova Mariana <drozdova.mariana93@gmail.com>
2022-06-16 16:05:56 +0200

    rewrite Product component and remove  type

--- ClientApp/src/components/Product.tsx
+++ ClientApp/src/components/Product.tsx
@@ -1,35 +1,22 @@
-﻿import { Component } from 'react';
-
+﻿import { IProduct } from '../pages/Home/ProductList';
 import './Product.css';
 
-interface IProps {
-    src: string,
-    alt: string,
-    title: string,
-    stock?: number,
-    price: number,
-    montage?: number
-}
+const Product = ({ src, alt, title, stock, price, montage }: IProduct) => {
+  return (
+    <div className="product-container">
+      <img className="w-100" src={src} alt={alt} />
+      <div className="container-content">
+        <h3>{title}</h3>
+        {stock ? <p className="m-0">{`Skladom: ${stock}`}</p> : ''}
+        <p className="m-0">{`Cena(1ks): ${price} EUR`}</p>
+        {montage ? (
+          <p className="m-0">{`Montáž + demontáž(1ks): ${montage} EUR`}</p>
+        ) : (
+          ''
+        )}
+      </div>
+    </div>
+  );
+};
 
-export default class Product extends Component<IProps> {
-    static displayName = Product.name;
-
-    constructor(props: IProps) {
-        super(props);
-    }
-
-    render() {
-        const { src, alt, title, stock, price, montage } = this.props;
-        return (
-            <div className="product-container">
-                <img className="w-100" src={src} alt={alt} />
-                <div className="container-content">
-                    <h3>{title}</h3>
-                    {(stock) ? < p className="m-0">{`Skladom: ${stock}`}</p> : ""}                    
-                    <p className="m-0">{`Cena(1ks): ${price} EUR`}</p>
-                    {(montage) ? <p className="m-0">{`Montáž + demontáž(1ks): ${montage} EUR`}</p> : ""}
-                </div>
-            </div>
-        );
-    }
-}
+export default Product;
